a422b2154eeefbc635f5ce0cc6f1d72c1b83d28e
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-04-23 16:59:49 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsRef = document.querySelector('#ingredients')
+
+const ingredientsList = ingredients.map((element) => {
+  const elementList = document.createElement('li');
+  elementList.classList.add('item');
+  elementList.textContent = element;
+  return elementList
+})
+
+ingredientsRef.append(...ingredientsList)
