89750c52147eacd42a08b8bbd600c6480c3c85aa
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-04-25 00:33:17 +0300

    task-4

---
+++ js/task-04.js
@@ -0,0 +1,30 @@
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const valueRef = document.querySelector('#value')
+let countValue = 0;
+
+const updateValue = () => {
+    valueRef.textContent = countValue
+};
+const increment = () => {
+    countValue += 1
+};
+const decrement = () => {
+    countValue -= 1
+}
+incrementBtn.addEventListener('click', () => {
+    increment();
+    updateValue()
+
+});
+decrementBtn.addEventListener('click', () => {
+    decrement();
+    updateValue()
+})
+
+
+
+
+
+
+
