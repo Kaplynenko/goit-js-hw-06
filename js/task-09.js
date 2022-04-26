bc0bf2848358699be0b6a6279a0e7e960a6172f3
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-04-26 22:00:01 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const changeColorBtn = document.querySelector('.change-color');
+const bodyRef = document.querySelector('body')
+const velueColor = document.querySelector('.color')
+
+changeColorBtn.addEventListener('click', changeColor)
+function changeColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+velueColor.textContent = getRandomHexColor()
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
