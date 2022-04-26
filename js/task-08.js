05318e992848212bd9745e772ecb654f011ccf54
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-04-26 19:57:01 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
 
+const form = document.querySelector('.login-form')
+
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert('заполните ВСЕ поля')
+  }
+
+  console.log({Email:email.value, Password:password.value});
+  event.currentTarget.reset();
+}
+
+
+
+
+
